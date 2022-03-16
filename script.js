9beefb4e8d49764651ba106ff2ec890d5e12981c
neonritual <vtshore@gmail.com>
2022-03-16 21:27:15 +0900

    Add color change and clear button functionality.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
+// Generate random colors
 const hexValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
-
-function changeHex() {
+function randomColorHex() {
   let hex = "#";
 
   for (let i = 0; i < 6; i++) {
@@ -11,35 +11,20 @@ function changeHex() {
   return hex;
 }
 
-console.log(changeHex());
-
-// for (let columns = 0; columns < 40; columns++) {
-//   for (let rows = 0; rows < 0; rows++) {
-//     let div = document.createElement("div");
-//     div.style.width = "10px";
-//     div.style.height = "10px";
-//     div.textContent = "a";
-//     div.style.background = changeHex();
-//     document.getElementById("sketchpad").appendChild(div);
-//   }
-//   let br = document.createElement("br");
-//   document.getElementById("sketchpad").appendChild(br);
-// }
-
+// Get sizes for the sketchpad grid: how many cells across, the width of the sketchpad on the screen, the size of each cell.
+let pixelNumber = 16;
 let sketchpadWidth = document.getElementById("sketchpad").clientWidth;
-let cellSize = sketchpadWidth / 40 + "px";
+let cellSize = sketchpadWidth / pixelNumber + "px";
 
-console.log(sketchpadWidth);
-
-for (var i = 0; i < 40; i++) {
+// Create the sketchpad grid.
+for (var i = 0; i < pixelNumber; i++) {
   var row = document.createElement("div");
   row.style.flex = "1";
-  for (var j = 0; j < 40; j++) {
+  for (var j = 0; j < pixelNumber; j++) {
     var cell = document.createElement("div");
     cell.style.width = cellSize;
     cell.style.height = cellSize;
-    cell.classList.add("pixel", "inactive");
-
+    cell.classList.add("pixel");
     cell.style.flex = "1";
     row.appendChild(cell);
   }
@@ -47,19 +32,44 @@ for (var i = 0; i < 40; i++) {
 }
 
 let pixels = document.getElementsByClassName("pixel");
-
-// Array.from(pixels).forEach(function (pixel) {
-//   pixel.addEventListener("mouseover", function (flipped) {
-//     (penned.target.style.backgroundColor = "blue"), false;
-//   });
-// });
-
-Array.from(pixels).forEach(function (pixel) {
-  pixel.addEventListener("mouseenter", function () {
-    pixel.classList.add("active-black");
+// Default mouseover color change effect on cells.
+[].forEach.call(pixels, function (pixel) {
+  pixel.addEventListener("mouseover", function (item) {
+    item.target.style.backgroundColor = "black";
   });
 });
 
-// pixel.addEventListener("mouseenter", function (flip) {
-//   (flip.target.style.backgroundColor = "blue"), false;
+// Generate random colors.
+let randomColorButton = document.getElementById("randomColorButton");
+randomColorButton.addEventListener("click", function () {
+  [].forEach.call(pixels, function (pixel) {
+    pixel.addEventListener("mouseover", function (item) {
+      item.target.style.backgroundColor = randomColorHex();
+    });
+  });
+});
+
+// Generate only black again.
+let blackButton = document.getElementById("blackButton");
+blackButton.addEventListener("click", function () {
+  [].forEach.call(pixels, function (pixel) {
+    pixel.addEventListener("mouseover", function (item) {
+      item.target.style.backgroundColor = "black";
+    });
+  });
+});
+
+// Clear the grid.
+let clearButton = document.getElementById("clearButton");
+clearButton.addEventListener("click", function () {
+  for (var i = 0, max = pixels.length; i < max; i++) {
+    pixels[i].style.backgroundColor = "white";
+  }
+});
+
+// // THIS WORKED, but is adding a class to change the color.
+// Array.from(pixels).forEach(function (pixel) {
+//   pixel.addEventListener("mouseenter", function () {
+//     pixel.classList.add("active-black");
+//   });
 // });
